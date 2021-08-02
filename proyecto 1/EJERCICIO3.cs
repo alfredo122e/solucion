f51a09c09654c1b5c794f09ba96e4a725f7ff89d
Adony Ulloa <adonyulloa@gmail.com>
2021-08-02 16:13:09 -0600

    Yo Edwin Adony Ulloa Diaz comienzo a trabajar en los demas 3 ejercicios

---
+++ proyecto 1/EJERCICIO3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_1
+{
+    class EJERCICIO3
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
